17055d79e40b3387fc71894f21f19305ccf48f94
Freddy <fredericdouamba2014@gmail.com>
2023-11-16 13:53:08 -0800

    Initial Commit

---
+++ DetectionMotion/DetectionMotionApp.swift
@@ -0,0 +1,17 @@
+//
+//  DetectionMotionApp.swift
+//  DetectionMotion
+//
+//  Created by Student Account on 11/16/23.
+//
+
+import SwiftUI
+
+@main
+struct DetectionMotionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
